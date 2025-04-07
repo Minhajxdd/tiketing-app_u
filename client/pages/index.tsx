090830c15a6703c1b0112273f079da057285b3ff
Mohammed Minhaj <nijulinux@gmail.com>
2025-04-07 22:48:41 +0530

    fix(client): fixed

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,13 @@
-import axios from 'axios';
-import config from '../config/configuration';
+import buildClient from "../api/build-client";
 
-const LandingPage = () => {
-    // const response = axios.get(`/api/users/currentuser`, {withCredentials: true});
- 
-    return <h1>Landing Page</h1>;
-}
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? <h1>You are signed in </h1> : <h1>You are Not signed In</h1>;
+};
 
-// LandingPage.getInitialProps = async () => {
-//     // return response.data;
-// }
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
+};
 
 export default LandingPage;
