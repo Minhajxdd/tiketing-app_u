12f20cf7120de6d3a47073ec16baf9bcd9cb69b4
Mohammed Minhaj <nijulinux@gmail.com>
2025-04-08 20:06:22 +0530

    Initial commit

---
+++ tickets/src/app.ts
@@ -0,0 +1,30 @@
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+const app = express();
+app.set("trust proxy", true);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    signed: false,
+    // secure: process.env.NODE_ENV !== 'test',
+    secure: false,
+    sameSite: "lax",
+  })
+);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(req.method, " ", req.url);
+  next();
+});
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
