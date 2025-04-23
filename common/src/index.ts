7a6d73e4073ae8f4b31adc2edd1c564cd0dab951
Mohammed Minhaj <nijulinux@gmail.com>
2025-04-23 21:58:29 +0530

    feat: new events added

--- common/src/index.ts
+++ common/src/index.ts
@@ -16,3 +16,5 @@ export * from "./events/subjects";
 export * from "./events/ticket-created-events";
 export * from "./events/ticket-updated-event";
 export * from "./events/types/order-status";
+export * from "./events/ticket-created-events";
+export * from "./events/ticket-updated-event";
