d8630d99c3f522ed950c62565138c1d1aa3cba9b
Mohammed Minhaj <nijulinux@gmail.com>
2025-04-29 20:14:29 +0530

    fix: order created listen

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,47 @@
+import {
+  Listener,
+  OrderCreatedEvent,
+  OrderStatus,
+  Subjects,
+} from "@tkgtickets/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/tickets";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName: string = queueGroupName;
+
+  async onMessage(
+    data: {
+      id: string;
+      version: number;
+      status: OrderStatus;
+      userId: string;
+      expiresAt: string;
+      ticket: { id: string; price: number };
+    },
+    msg: Message
+  ): Promise<void> {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    ticket.set({ orderId: data.id });
+
+    await ticket.save();
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
+
+    msg.ack();
+  }
+}
