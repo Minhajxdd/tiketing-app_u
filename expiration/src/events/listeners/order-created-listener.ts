796a170de78aa2a87b63eafcd2d3131477239b0f
Mohammed Minhaj <nijulinux@gmail.com>
2025-04-30 11:49:05 +0530

    feat(expiration): listener updated

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -6,6 +6,7 @@ import {
 } from "@tkgtickets/common";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -22,5 +23,18 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
       ticket: { id: string; price: number };
     },
     msg: Message
-  ): Promise<void> {}
+  ): Promise<void> {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      }
+    );
+
+    msg.ack();
+  }
 }
