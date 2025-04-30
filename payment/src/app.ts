d300b31babdf70132df2ad92c7a7a067f91b9816
Mohammed Minhaj <nijulinux@gmail.com>
2025-04-30 20:01:49 +0530

    feat: updated src and dependencies

---
+++ payment/src/app.ts
@@ -0,0 +1,33 @@
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { currentUser, errorHandler, NotFoundError } from "@tkgtickets/common";
+
+const app = express();
+app.set("trust proxy", true);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    signed: false,
+    // secure: process.env.NODE_ENV !== 'test',
+    secure: false,
+    sameSite: "lax",
+  })
+);
+
+app.use(currentUser);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(req.method, " ", req.url);
+  next();
+});
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
