d49abc0ebf7c45a50778ca2afa0c90d8a8afdd68
Mohammed Minhaj <nijulinux@gmail.com>
2025-05-01 15:49:16 +0530

    feat:(payments): added payment created publisher

--- expiration/src/events/publishers/expiration-complete-publisher.ts
+++ expiration/src/events/publishers/expiration-complete-publisher.ts
@@ -1,7 +1,9 @@
-import { ExpirationCompleteEvent, Publisher, Subjects } from "@tkgtickets/common";
+import {
+  ExpirationCompleteEvent,
+  Publisher,
+  Subjects,
+} from "@tkgtickets/common";
 
 export class ExpirationCompletePublisher extends Publisher<ExpirationCompleteEvent> {
-    subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
-
-    
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
 }
