b41b1887435d69b561da00986cdaa86065745930
Mohammed Minhaj <nijulinux@gmail.com>
2025-05-01 12:34:16 +0530

    feat(payments): order created done

---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,38 @@
+import {
+  Listener,
+  OrderCreatedEvent,
+  OrderStatus,
+  Subjects,
+} from "@tkgtickets/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName: string = queueGroupName;
+
+  async onMessage(
+    data: {
+      id: string;
+      version: number;
+      status: OrderStatus;
+      userId: string;
+      expiresAt: string;
+      ticket: { id: string; price: number };
+    },
+    msg: Message
+  ): Promise<void> {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version,
+    });
+
+    await order.save();
+
+    msg.ack();
+  }
+}
